472b30b9202d543f9c14609d952fe889598db860
unknown <digvijaykachhawaha0087@gmail.com>
2024-06-20 14:51:42 +0530

    Setup completed

---
+++ NoraLure/app/profile.jsx
@@ -0,0 +1,13 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+const Profile = () => {
+  return (
+    <View>
+      <Text>Hey!!</Text>
+    </View>
+  )
+}
+
+export default Profile
+
